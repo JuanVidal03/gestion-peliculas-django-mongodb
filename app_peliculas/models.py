c739b208a27268008273f039f8612a5ec8f058ee
Juan Vidal <juanmvg2003@gmail.com>
2024-05-21 21:17:07 -0500

    fueron creadas las migraciones

--- app_peliculas/models.py
+++ app_peliculas/models.py
@@ -1,3 +1,24 @@
-from django.db import models
+from djongo import models
 
-# Create your models here.
+
+# modelo del genero de las peliculas  
+class Genero(models.Model):
+    _id = models.ObjectIdField()
+    nombre = models.CharField(max_length=50,unique=True)
+    
+    def __str__(self)->str:
+        return self.nombre
+
+
+# modelo de las peliculas
+class Pelicula(models.Model):
+    _id = models.ObjectIdField()
+    codigo = models.CharField(max_length=9)
+    titulo = models.CharField(max_length=50)
+    protagonista = models.CharField(max_length=50)
+    duracion = models.IntegerField()
+    resumen = models.CharField(max_length=2000)
+    genero = models.ForeignKey(Genero, on_delete=models.PROTECT)
+    
+    def __str__(self)->str:
+        return self.titulo
--- gestion_peliculas_mongodb/settings.py
+++ gestion_peliculas_mongodb/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # aplicaciones creadas
+    'app_peliculas'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,10 @@ WSGI_APPLICATION = 'gestion_peliculas_mongodb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'gestion_peliculas_django_mongodb',
+        'HOST': 'localhost',
+        'PORT': 27017
     }
 }
 
